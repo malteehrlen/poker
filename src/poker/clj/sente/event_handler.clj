cb5fae406364968da2d011339bd77c06a29870cc
Malte Ehrlen <malte@ehrlen.com>
2021-03-07 22:27:00 +0100

    comment formatting

--- src/poker/clj/sente/event_handler.clj
+++ src/poker/clj/sente/event_handler.clj
@@ -7,53 +7,19 @@
 
 (defonce broadcast-enabled?_ (atom true))
 
-(defn start-example-broadcaster!
-  "As an example of server>user async pushes, setup a loop to broadcast an
-      event to all connected users every 10 seconds"
-  []
-  (let [broadcast!
-        (fn [i]
-          (let [uids (:any @connected-uids)]
-            (debugf "Broadcasting server>user: %s uids" (count uids))
-            (doseq [uid uids]
-              (chsk-send! uid
-                          [:some/broadcast
-                           {:what-is-this "An async broadcast pushed from server"
-                            :how-often "Every 10 seconds"
-                            :to-whom uid
-                            :i i}]))))]
-
-    (go-loop [i 0]
-      (<! (async/timeout 10000))
-      (when @broadcast-enabled?_ (broadcast! i))
-      (recur (inc i)))))
-;;;; Sente event handlers
-
 (defmulti event
   "Multimethod to handle Sente `event-msg`s"
-  :id                                               ; Dispatch on event-id
-  )
-
-(defn wrapped-event
-  "Wraps `event` with logging, error catching, etc."
-  [{:as ev-msg :keys [id ?data event]}]
-  (event ev-msg)                           ; Handle event-msgs on a single thread
-      ;; (future (event ev-msg)) ; Handle event-msgs on a thread pool
+  :id ; Dispatch on event-id
   )
 
 (defmethod event
-  :default                                         ; Default/fallback case (no other matching handler)
+  :default ; Default/fallback case (no other matching handler)
   [{:as ev-msg :keys [event id ?data ring-req ?reply-fn send-fn]}]
   (let [session (:session ring-req)
         uid (:uid session)]
     (debugf "Unhandled event: %s" event)
     (when ?reply-fn
       (?reply-fn {:umatched-event-as-echoed-from-from-server event}))))
-
-(defmethod event :example/toggle-broadcast
-  [{:as ev-msg :keys [?reply-fn]}]
-  (let [loop-enabled? (swap! broadcast-enabled?_ not)]
-    (?reply-fn loop-enabled?)))
 
 (defmethod event :chsk/uidport-open [{:keys [uid client-id]}]
   (println "New connection:" uid client-id))
