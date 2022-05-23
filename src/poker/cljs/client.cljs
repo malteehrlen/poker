75b80bd501f849f4d70de9bf46e6748db0193c66
Malte Ehrlen <malte.ehrlen@freiheit.com>
2022-05-23 12:27:19 +0200

    add form submit, description, author email and donate link

--- src/poker/cljs/client.cljs
+++ src/poker/cljs/client.cljs
@@ -38,6 +38,7 @@
       [:span.main {}
        [:h1 "P O K E R M A N C E R"]
        [:div.content
+        [:form
         [:input
          {:type "form"
           :placeholder "nickname"
@@ -48,7 +49,14 @@
           :placeholder "room"
           :value @room-id
           :on-change    #(reset! room-id (.. % -target -value))}]
-        [:button {:on-click #(handle-room-open @room-id @user-id)} "start pokering"]]])))
+        [:button {:type "submit" :on-click #(handle-room-open @room-id @user-id)} "start pokering"]]
+        [:div.description
+        [:p.description "Pokermancer is a story poker tool for scrum teams. Unlike the vast majority of other poker tools, it isnt overdesigned or unstable. Pokermancer doesnt require any sso or signup."] 
+        [:p.description "To get started, join a room and share the url with your colleagues. There is no configurability at the moment, so if you are missing vital features feel free to shoot me an email (address below)."]]
+          [:p.back 
+           [:a {:href "https://www.paypal.com/donate/?hosted_button_id=EJ6PDT5HB8Q4C" :target "_blank" :rel "noopener noreferrer"} "Donate if you want"] 
+           [:br] "Written by malte@ehrlen.com" ]
+        ]])))
 
 (defn room-page []
   (let [routing-data (session/get :route)
@@ -63,7 +71,9 @@
          [:div.content
           [:h1 (str room-id)]
           [room-component]
-          [:p.back [:a {:on-click #(leave-room) :href (path-for :index)} "Back to the lobby"]]]]))))
+          [:p.back 
+           [:a {:on-click #(leave-room) :href (path-for :index)} "Back to the lobby"][:br] 
+           [:a {:href "https://www.paypal.com/donate/?hosted_button_id=EJ6PDT5HB8Q4C" :target "_blank" :rel "noopener noreferrer"} "Donate if you want"] [:br] "Written by malte@ehrlen.com" ]]]))))
 
 ;; -------------------------
 ;; Translate routes -> page components
